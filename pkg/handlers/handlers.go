69ad143e743f5296ad431c93c057d4a5c7109e2d
Muhammet Burak Golec <mozartburak42@gmail.com>
2024-01-23 14:48:53 +0200

    Add: Ip track test in handler

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,12 +1,18 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/muhammetburakgolec/burakgolec.com-go/pkg/render"
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
+	ip := r.RemoteAddr
+	if forwarded := r.Header.Get("X-Forwarded-For"); forwarded != "" {
+		ip = forwarded
+	}
+	log.Printf("Gelen istek IP: %s\n", ip)
 	render.RenderTemplate(w, "home.html")
 }
 
