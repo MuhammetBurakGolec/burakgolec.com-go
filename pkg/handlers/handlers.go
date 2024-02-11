e322ca7e78b87223d94fb768de27c2bdf6f22d8b
Muhammet Burak Golec <mozartburak42@gmail.com>
2024-02-11 15:20:37 +0300

    Update: handlers.go

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -8,22 +8,29 @@ import (
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
+	ip_track(r)
+	render.RenderTemplate(w, "home.html")
+}
+
+func About(w http.ResponseWriter, r *http.Request) {
+	ip_track(r)
+	render.RenderTemplate(w, "about.html")
+}
+
+func Features(w http.ResponseWriter, r *http.Request) {
+	ip_track(r)
+	render.RenderTemplate(w, "features.html")
+}
+
+func Faqs(w http.ResponseWriter, r *http.Request) {
+	ip_track(r)
+	render.RenderTemplate(w, "faqs.html")
+}
+
+func ip_track(r *http.Request) {
 	ip := r.RemoteAddr
 	if forwarded := r.Header.Get("X-Forwarded-For"); forwarded != "" {
 		ip = forwarded
 	}
 	log.Printf("Gelen istek IP: %s\n", ip)
-	render.RenderTemplate(w, "home.html")
-}
-
-func About(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "about.html")
-}
-
-func Features(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "features.html")
-}
-
-func Faqs(w http.ResponseWriter, r *http.Request) {
-	render.RenderTemplate(w, "faqs.html")
 }
